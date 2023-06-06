12b6e2f04656648cc7aabb381bd8ddc549047745
Kavi Gupta <kavi@berkeley.edu>
2023-06-05 22:39:20 -0400

    update

--- setup.py
+++ setup.py
@@ -14,11 +14,13 @@ setuptools.setup(
     url="https://github.com/kavigupta/neurosym-lib",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.7",
     install_requires=[],
 )
