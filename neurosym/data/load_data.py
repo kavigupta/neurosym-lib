0e702f81a83a21865229b536c9dfbd9d4988a856
Kavi Gupta <kavi@berkeley.edu>
2023-08-14 03:10:34 -0400

    add some basic dataset framework

---
+++ neurosym/data/load_data.py
@@ -0,0 +1,143 @@
+import io
+import requests
+
+import torch
+import pytorch_lightning as pl
+
+from permacache import permacache
+import numpy as np
+
+
+def get_raw_url(github_folder, filename):
+    """
+    Get the raw url for a file in a github folder.
+
+    Parameters
+    ----------
+    github_folder : str
+        The github folder containing the file. As a url.
+            E.g., github_folder = https://github.com/trishullab/near/tree/master/near_code/data/example
+    filename : str
+        The name of the file.
+            E.g., filename = test_ex_data.npy
+
+    Returns
+    -------
+    raw_url : str
+        The raw url for the file.
+            E.g., https://github.com/trishullab/near/raw/master/near_code/data/example/test_ex_data.npy
+    """
+    raw_url = github_folder.replace("tree", "raw") + "/" + filename
+    return raw_url
+
+
+@permacache("neurosym/data/load_data/load_npy")
+def load_npy(url):
+    """
+    Load a numpy file from a url.
+
+    Parameters
+    ----------
+    url : str
+        The url of the numpy file.
+
+    Returns
+    -------
+    data : np.ndarray
+        The data in the numpy file.
+    """
+    data = requests.get(url).content
+    data = np.load(io.BytesIO(data))
+    return data
+
+
+class DatasetFromNpy(torch.utils.data.Dataset):
+    """
+    A dataset from an array, loaded from a url.
+
+    TODO test/val split
+    """
+
+    def __init__(self, input_url, output_url, seed):
+        """
+        Parameters
+        ----------
+        url : str
+            The url of the numpy file.
+        """
+        self.inputs = load_npy(input_url)
+        self.outputs = load_npy(output_url)
+        assert len(self.inputs) == len(self.outputs)
+        if seed is not None:
+            self.ordering = np.random.RandomState(seed=seed).permutation(
+                len(self.inputs)
+            )
+        else:
+            self.ordering = np.arange(len(self.inputs))
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, idx):
+        return self.inputs[self.ordering[idx]], self.outputs[self.ordering[idx]]
+
+
+class DatasetWrapper(pl.LightningDataModule):
+    def __init__(
+        self,
+        train: torch.utils.data.Dataset,
+        test: torch.utils.data.Dataset,
+        batch_size: int = 32,
+    ):
+        super().__init__()
+        self.train = train
+        self.test = test
+        self.batch_size = batch_size
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(self.train, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(self.test, batch_size=self.batch_size)
+
+
+def numpy_dataset_from_github(
+    github_url,
+    train_input_path,
+    train_output_path,
+    test_input_path,
+    test_output_path,
+):
+    """
+    Load a dataset from a github url.
+
+    Parameters
+    ----------
+    github_url : str
+        The url of the github folder containing the data.
+    train_input_path : str
+        The path to the training input data.
+    train_output_path : str
+        The path to the training output data.
+    test_input_path : str
+        The path to the test input data.
+    test_output_path : str
+        The path to the test output data.
+
+    Returns
+    -------
+    dataset : function seed -> DatasetWrapper
+        The dataset, as a function of the seed.
+    """
+    return lambda train_seed: DatasetWrapper(
+        DatasetFromNpy(
+            get_raw_url(github_url, train_input_path),
+            get_raw_url(github_url, train_output_path),
+            train_seed,
+        ),
+        DatasetFromNpy(
+            get_raw_url(github_url, test_input_path),
+            get_raw_url(github_url, test_output_path),
+            None,
+        ),
+    )
---
+++ neurosym/examples/datasets/near_data_example.py
@@ -0,0 +1,10 @@
+from neurosym.data.load_data import numpy_dataset_from_github
+
+
+data = numpy_dataset_from_github(
+    "https://github.com/trishullab/near/tree/master/near_code/data/example",
+    "train_ex_data.npy",
+    "train_ex_labels.npy",
+    "test_ex_data.npy",
+    "test_ex_labels.npy",
+)
