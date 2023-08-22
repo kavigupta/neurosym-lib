e94b33250d207d533a7f2710d5bd11f1d637280d
Kavi Gupta <kavi@berkeley.edu>
2023-08-21 20:03:27 -0400

    add mutable arith combinators example

---
+++ neurosym/examples/mutable_arith_combinators.py
@@ -0,0 +1,46 @@
+from neurosym.dsl.dsl_factory import DSLFactory
+from neurosym.types.type_string_repr import TypeDefiner
+
+
+def increment_and_return(counter):
+    counter[0] += 1
+    return counter[0]
+
+
+t = TypeDefiner()
+t.typedef("fn", "(i) -> i")
+
+dslf = DSLFactory()
+dslf.typedef("fn", "(i) -> i")
+dslf.concrete(
+    "x",
+    "() -> $fn",
+    lambda: lambda t: t,
+)
+
+dslf.concrete(
+    "1",
+    "() -> $fn",
+    lambda: lambda t: 1,
+)
+
+dslf.concrete(
+    "+",
+    "($fn, $fn) -> $fn",
+    lambda x, y: lambda t: x(t) + y(t),
+)
+
+dslf.concrete(
+    "*",
+    "($fn, $fn) -> $fn",
+    lambda x, y: lambda t: x(t) * y(t),
+)
+
+dslf.parameterized(
+    "count",
+    "() -> $fn",
+    lambda counter: lambda t: increment_and_return(counter),
+    dict(counter=lambda: [0]),
+)
+
+mutable_arith_combinators = dslf.finalize()
