e53c68d134c7590e43043b1990b453a8db9d3259
Kavi Gupta <kavi@berkeley.edu>
2023-09-04 22:41:04 -0400

    add check for lam

--- neurosym/dsl/dsl.py
+++ neurosym/dsl/dsl.py
@@ -22,6 +22,10 @@ class DSL:
             assert (
                 production.symbol() not in symbols
             ), f"Duplicate symbol {production.symbol()}"
+            if production.symbol().startswith("lam"):
+                raise ValueError(
+                    f"Symbol {production.symbol()} starts with lam, which is reserved"
+                )
             symbols.add(production.symbol())
         self._production_by_symbol = {
             production.symbol(): production for production in self.productions
