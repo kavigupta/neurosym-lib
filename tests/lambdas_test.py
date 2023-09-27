80c6b2eef7f6601c8e23b8b5e5170a91f250b24e
Kavi Gupta <kavi@berkeley.edu>
2023-09-26 21:58:05 -0400

    fix tests

--- tests/lambdas_test.py
+++ tests/lambdas_test.py
@@ -1,6 +1,5 @@
 import itertools
 import unittest
-from neurosym.dsl.dsl_factory import DSLFactory
 
 
 from neurosym.examples.basic_arith import basic_arith_dsl
@@ -17,6 +16,26 @@ from neurosym.programs.s_expression_render import (
 dsl = basic_arith_dsl(True)
 
 
+class TestPruning(unittest.TestCase):
+    def test_output(self):
+        expected = """
+            + :: (i, i) -> i
+            1 :: () -> i
+        lam_0 :: L<i -> i|i;i> -> (i, i) -> i -> i
+        lam_1 :: L<i|i;i> -> (i, i) -> i
+        lam_2 :: L<i -> i|i> -> i -> i -> i
+        lam_3 :: L<i|i> -> i -> i
+        $0_0 :: V<i@0>
+        $1_1 :: V<i@1>
+        $2_2 :: V<i@2>
+        """
+        actual = dsl.render()
+        self.assertEqual(
+            {line.strip() for line in actual.strip().split("\n")},
+            {line.strip() for line in expected.strip().split("\n")},
+        )
+
+
 class TestEvaluate(unittest.TestCase):
     def evaluate(self, code):
         return dsl.compute(
@@ -24,53 +43,53 @@ class TestEvaluate(unittest.TestCase):
         )
 
     def test_constant(self):
-        fn = self.evaluate("(lam_167 (1))")
+        fn = self.evaluate("(lam_3 (1))")
         for i in range(10):
             self.assertEqual(fn(i), 1)
 
     def test_identity(self):
-        fn = self.evaluate("(lam_167 ($0_16))")
+        fn = self.evaluate("(lam_3 ($0_0))")
         for i in range(10):
             self.assertEqual(fn(i), i)
 
     def test_add_1(self):
-        fn = self.evaluate("(lam_167 (+ (1) ($0_16)))")
+        fn = self.evaluate("(lam_3 (+ (1) ($0_0)))")
         for i in range(10):
             self.assertEqual(fn(i), i + 1)
 
     def test_double_constant(self):
-        fn = self.evaluate("(lam_166 (lam_167 (1)))")
+        fn = self.evaluate("(lam_2 (lam_3 (1)))")
         for i in range(10):
             for j in range(10):
                 self.assertEqual(fn(i)(j), 1)
 
     def test_get_inner_var(self):
-        fn = self.evaluate("(lam_166 (lam_167 ($0_16)))")
+        fn = self.evaluate("(lam_2 (lam_3 ($0_0)))")
         for i in range(10):
             for j in range(10):
                 self.assertEqual(fn(i)(j), j)
 
     def test_get_outer_var(self):
-        fn = self.evaluate("(lam_166 (lam_167 ($1_17)))")
+        fn = self.evaluate("(lam_2 (lam_3 ($1_1)))")
         for i in range(10):
             for j in range(10):
                 self.assertEqual(fn(i)(j), i)
 
     def test_sub_curried(self):
-        fn = self.evaluate("(lam_166 (lam_167 (+ (+ ($0_16) ($0_16)) ($1_17))))")
+        fn = self.evaluate("(lam_2 (lam_3 (+ (+ ($0_0) ($0_0)) ($1_1))))")
         for i in range(10):
             for j in range(10):
                 self.assertEqual(fn(i)(j), i + 2 * j)
 
     def test_two_arg_const(self):
-        fn = self.evaluate("(lam_162 (1))")
+        fn = self.evaluate("(lam_1 (1))")
         for i in range(10):
             for j in range(10):
                 self.assertEqual(fn(i, j), 1)
 
     def test_two_arg_sub(self):
         # note that the order of arguments is reversed here
-        fn = self.evaluate("(lam_162 (+ (+ ($0_16) ($0_16)) ($1_17)))")
+        fn = self.evaluate("(lam_1 (+ (+ ($0_0) ($0_0)) ($1_1)))")
         for i in range(10):
             for j in range(10):
                 self.assertEqual(fn(i, j), 2 * i + j)
@@ -78,7 +97,7 @@ class TestEvaluate(unittest.TestCase):
     def test_three_arg_sub(self):
         # note that the order of arguments is reversed here
         fn = self.evaluate(
-            "(lam_161 (lam_167 (+ (+ ($2_18) (+ ($2_18) ($2_18))) (+ (+ ($0_16) ($0_16)) ($1_17)))))"
+            "(lam_0 (lam_3 (+ (+ ($2_2) (+ ($2_2) ($2_2))) (+ (+ ($0_0) ($0_0)) ($1_1)))))"
         )
         for i in range(10):
             for j in range(10):
@@ -109,16 +128,16 @@ class TestEnumerate(unittest.TestCase):
         self.assertEnumerateType(
             "i -> i",
             [
-                "(lam_167 (1))",
-                "(lam_167 ($0_16))",
-                "(lam_167 (+ (1) (1)))",
-                "(lam_167 (+ (1) ($0_16)))",
-                "(lam_167 (+ ($0_16) (1)))",
-                "(lam_167 (+ ($0_16) ($0_16)))",
-                "(lam_167 (+ (+ (1) (1)) (1)))",
-                "(lam_167 (+ (+ (1) (1)) ($0_16)))",
-                "(lam_167 (+ (+ (1) ($0_16)) (1)))",
-                "(lam_167 (+ (+ (1) ($0_16)) ($0_16)))",
+                "(lam_3 (1))",
+                "(lam_3 ($0_0))",
+                "(lam_3 (+ (1) (1)))",
+                "(lam_3 (+ (1) ($0_0)))",
+                "(lam_3 (+ ($0_0) (1)))",
+                "(lam_3 (+ ($0_0) ($0_0)))",
+                "(lam_3 (+ (+ (1) (1)) (1)))",
+                "(lam_3 (+ (+ (1) (1)) ($0_0)))",
+                "(lam_3 (+ (+ (1) ($0_0)) (1)))",
+                "(lam_3 (+ (+ (1) ($0_0)) ($0_0)))",
             ],
         )
 
@@ -126,16 +145,16 @@ class TestEnumerate(unittest.TestCase):
         self.assertEnumerateType(
             "i -> i -> i",
             [
-                "(lam_166 (lam_167 (1)))",
-                "(lam_166 (lam_167 ($0_16)))",
-                "(lam_166 (lam_167 ($1_17)))",
-                "(lam_166 (lam_167 (+ (1) (1))))",
-                "(lam_166 (lam_167 (+ (1) ($0_16))))",
-                "(lam_166 (lam_167 (+ (1) ($1_17))))",
-                "(lam_166 (lam_167 (+ ($0_16) (1))))",
-                "(lam_166 (lam_167 (+ ($0_16) ($0_16))))",
-                "(lam_166 (lam_167 (+ ($0_16) ($1_17))))",
-                "(lam_166 (lam_167 (+ ($1_17) (1))))",
+                "(lam_2 (lam_3 (1)))",
+                "(lam_2 (lam_3 ($0_0)))",
+                "(lam_2 (lam_3 ($1_1)))",
+                "(lam_2 (lam_3 (+ (1) (1))))",
+                "(lam_2 (lam_3 (+ (1) ($0_0))))",
+                "(lam_2 (lam_3 (+ (1) ($1_1))))",
+                "(lam_2 (lam_3 (+ ($0_0) (1))))",
+                "(lam_2 (lam_3 (+ ($0_0) ($0_0))))",
+                "(lam_2 (lam_3 (+ ($0_0) ($1_1))))",
+                "(lam_2 (lam_3 (+ ($1_1) (1))))",
             ],
         )
 
@@ -143,16 +162,16 @@ class TestEnumerate(unittest.TestCase):
         self.assertEnumerateType(
             "(i, i) -> i",
             [
-                "(lam_162 (1))",
-                "(lam_162 ($0_16))",
-                "(lam_162 ($1_17))",
-                "(lam_162 (+ (1) (1)))",
-                "(lam_162 (+ (1) ($0_16)))",
-                "(lam_162 (+ (1) ($1_17)))",
-                "(lam_162 (+ ($0_16) (1)))",
-                "(lam_162 (+ ($0_16) ($0_16)))",
-                "(lam_162 (+ ($0_16) ($1_17)))",
-                "(lam_162 (+ ($1_17) (1)))",
+                "(lam_1 (1))",
+                "(lam_1 ($0_0))",
+                "(lam_1 ($1_1))",
+                "(lam_1 (+ (1) (1)))",
+                "(lam_1 (+ (1) ($0_0)))",
+                "(lam_1 (+ (1) ($1_1)))",
+                "(lam_1 (+ ($0_0) (1)))",
+                "(lam_1 (+ ($0_0) ($0_0)))",
+                "(lam_1 (+ ($0_0) ($1_1)))",
+                "(lam_1 (+ ($1_1) (1)))",
             ],
         )
 
@@ -160,16 +179,16 @@ class TestEnumerate(unittest.TestCase):
         self.assertEnumerateType(
             "(i, i) -> i -> i",
             [
-                "(lam_161 (lam_167 ($0_16)))",
-                "(lam_161 (lam_167 ($1_17)))",
-                "(lam_161 (lam_167 ($2_18)))",
-                "(lam_161 (lam_167 (+ (1) ($0_16))))",
-                "(lam_161 (lam_167 (+ (1) ($1_17))))",
-                "(lam_161 (lam_167 (+ (1) ($2_18))))",
-                "(lam_161 (lam_167 (+ ($0_16) (1))))",
-                "(lam_161 (lam_167 (+ ($0_16) ($0_16))))",
-                "(lam_161 (lam_167 (+ ($0_16) ($1_17))))",
-                "(lam_161 (lam_167 (+ ($0_16) ($2_18))))",
+                "(lam_0 (lam_3 ($0_0)))",
+                "(lam_0 (lam_3 ($1_1)))",
+                "(lam_0 (lam_3 ($2_2)))",
+                "(lam_0 (lam_3 (+ (1) ($0_0))))",
+                "(lam_0 (lam_3 (+ (1) ($1_1))))",
+                "(lam_0 (lam_3 (+ (1) ($2_2))))",
+                "(lam_0 (lam_3 (+ ($0_0) (1))))",
+                "(lam_0 (lam_3 (+ ($0_0) ($0_0))))",
+                "(lam_0 (lam_3 (+ ($0_0) ($1_1))))",
+                "(lam_0 (lam_3 (+ ($0_0) ($2_2))))",
             ],
             filt=lambda x: "$" in str(x.program),
         )
