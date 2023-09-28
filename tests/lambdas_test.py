f672f2230740980e52a6ab6f2d7544ec461afe45
Kavi Gupta <kavi@berkeley.edu>
2023-09-27 21:10:58 -0400

    enumerate regression test

--- tests/lambdas_test.py
+++ tests/lambdas_test.py
@@ -17,6 +17,76 @@ from neurosym.programs.s_expression_render import (
 ba_dsl = basic_arith_dsl(True)
 
 
+class EnumerationRegressionTest(unittest.TestCase):
+    def assertRenderingEqual(self, actual, expected):
+        print(actual)
+        self.assertEqual(
+            {line.strip() for line in actual.strip().split("\n")},
+            {line.strip() for line in expected.strip().split("\n")},
+        )
+
+    def rendered_dsl(self):
+        dslf = DSLFactory()
+        dslf.known_types("i")
+        dslf.lambdas()
+        return dslf.finalize().render()
+
+    def test_basic(self):
+        self.assertRenderingEqual(
+            self.rendered_dsl(),
+            """
+            lam_0 :: L<(i, i) -> i|(i, i) -> i;(i, i) -> i> -> ((i, i) -> i, (i, i) -> i) -> (i, i) -> i
+            lam_1 :: L<i -> i|(i, i) -> i;(i, i) -> i> -> ((i, i) -> i, (i, i) -> i) -> i -> i
+            lam_2 :: L<i|(i, i) -> i;(i, i) -> i> -> ((i, i) -> i, (i, i) -> i) -> i
+            lam_3 :: L<(i, i) -> i|(i, i) -> i;i -> i> -> ((i, i) -> i, i -> i) -> (i, i) -> i
+            lam_4 :: L<i -> i|(i, i) -> i;i -> i> -> ((i, i) -> i, i -> i) -> i -> i
+            lam_5 :: L<i|(i, i) -> i;i -> i> -> ((i, i) -> i, i -> i) -> i
+            lam_6 :: L<(i, i) -> i|(i, i) -> i;i> -> ((i, i) -> i, i) -> (i, i) -> i
+            lam_7 :: L<i -> i|(i, i) -> i;i> -> ((i, i) -> i, i) -> i -> i
+            lam_8 :: L<i|(i, i) -> i;i> -> ((i, i) -> i, i) -> i
+            lam_9 :: L<(i, i) -> i|(i, i) -> i> -> ((i, i) -> i) -> (i, i) -> i
+            lam_10 :: L<i -> i|(i, i) -> i> -> ((i, i) -> i) -> i -> i
+            lam_11 :: L<i|(i, i) -> i> -> ((i, i) -> i) -> i
+            lam_12 :: L<(i, i) -> i|i -> i;(i, i) -> i> -> (i -> i, (i, i) -> i) -> (i, i) -> i
+            lam_13 :: L<i -> i|i -> i;(i, i) -> i> -> (i -> i, (i, i) -> i) -> i -> i
+            lam_14 :: L<i|i -> i;(i, i) -> i> -> (i -> i, (i, i) -> i) -> i
+            lam_15 :: L<(i, i) -> i|i -> i;i -> i> -> (i -> i, i -> i) -> (i, i) -> i
+            lam_16 :: L<i -> i|i -> i;i -> i> -> (i -> i, i -> i) -> i -> i
+            lam_17 :: L<i|i -> i;i -> i> -> (i -> i, i -> i) -> i
+            lam_18 :: L<(i, i) -> i|i -> i;i> -> (i -> i, i) -> (i, i) -> i
+            lam_19 :: L<i -> i|i -> i;i> -> (i -> i, i) -> i -> i
+            lam_20 :: L<i|i -> i;i> -> (i -> i, i) -> i
+            lam_21 :: L<(i, i) -> i|i -> i> -> (i -> i) -> (i, i) -> i
+            lam_22 :: L<i -> i|i -> i> -> (i -> i) -> i -> i
+            lam_23 :: L<i|i -> i> -> (i -> i) -> i
+            lam_24 :: L<(i, i) -> i|i;(i, i) -> i> -> (i, (i, i) -> i) -> (i, i) -> i
+            lam_25 :: L<i -> i|i;(i, i) -> i> -> (i, (i, i) -> i) -> i -> i
+            lam_26 :: L<i|i;(i, i) -> i> -> (i, (i, i) -> i) -> i
+            lam_27 :: L<(i, i) -> i|i;i -> i> -> (i, i -> i) -> (i, i) -> i
+            lam_28 :: L<i -> i|i;i -> i> -> (i, i -> i) -> i -> i
+            lam_29 :: L<i|i;i -> i> -> (i, i -> i) -> i
+            lam_30 :: L<(i, i) -> i|i;i> -> (i, i) -> (i, i) -> i
+            lam_31 :: L<i -> i|i;i> -> (i, i) -> i -> i
+            lam_32 :: L<i|i;i> -> (i, i) -> i
+            lam_33 :: L<(i, i) -> i|i> -> i -> (i, i) -> i
+            lam_34 :: L<i -> i|i> -> i -> i -> i
+            lam_35 :: L<i|i> -> i -> i
+            $0_0 :: V<(i, i) -> i@0>
+            $1_1 :: V<(i, i) -> i@1>
+            $2_2 :: V<(i, i) -> i@2>
+            $3_3 :: V<(i, i) -> i@3>
+            $0_4 :: V<i -> i@0>
+            $1_5 :: V<i -> i@1>
+            $2_6 :: V<i -> i@2>
+            $3_7 :: V<i -> i@3>
+            $0_8 :: V<i@0>
+            $1_9 :: V<i@1>
+            $2_10 :: V<i@2>
+            $3_11 :: V<i@3>
+            """,
+        )
+
+
 class TestPruning(unittest.TestCase):
     def test_output(self):
         expected = """
