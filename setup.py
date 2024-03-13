637acd008bf9f18eac7ae7fb8a780619699999a1
Kavi Gupta <kavi@berkeley.edu>
2024-03-13 15:18:17 -0400

    bump version (#52)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neurosym",
-    version="0.0.20",
+    version="0.0.21",
     author="Kavi Gupta",
     author_email="kavig+neurosym@mit.edu",
     description="Neurosymbolic library.",
