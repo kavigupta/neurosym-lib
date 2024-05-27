d2fb452cfeb10624176b51135eb52d4de31108c8
Atharva Sehgal <atharva.sehgal@gmail.com>
2024-05-27 16:12:14 +0000

    adding regression support

--- tests/tutorial/test_tutorial_1.py
+++ tests/tutorial/test_tutorial_1.py
@@ -7,9 +7,9 @@ from .utils import execute_notebook
 
 
 class TestTutorial1(unittest.TestCase):
-    def test_tutorial_1(self):
+    def test_tutorial_1_classification(self):
         result = execute_notebook(
-            "tutorial/tutorial1.ipynb",
+            "tutorial/tutorial1_classification.ipynb",
             suffix="import json; print('*' * 80); print(json.dumps(lin.weight.tolist()))",
         )
 
@@ -24,3 +24,17 @@ class TestTutorial1(unittest.TestCase):
         # representing an downward normal vector
         self.assertGreaterEqual(ang, -100)
         self.assertLessEqual(ang, -80)
+
+    def test_tutorial_1_regression(self):
+        """
+        Counts the number of modules in the solution. Should be more than 1 as a linear layer won't be able to approximate a non-linear function
+        """
+        result = execute_notebook(
+            "tutorial/tutorial1_regression.ipynb",
+            suffix="import json; print('*' * 80); print(json.dumps([len(module.contained_modules)]))",
+        )
+
+        *_, stars, len_module, _ = result.split("\n")
+        assert stars == "*" * 80
+        n_layers = json.loads(len_module)[0]
+        self.assertGreaterEqual(n_layers, 1)
