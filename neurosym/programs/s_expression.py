19faac11038fcbe43d6a8d14356ae363afaf630b
Kavi Gupta <kavi@berkeley.edu>
2024-05-27 22:28:00 -0400

    add replace methods

--- neurosym/programs/s_expression.py
+++ neurosym/programs/s_expression.py
@@ -13,6 +13,33 @@ class SExpression:
             yield from x.postorder
         yield self
 
+    def replace_symbols_by_id(self, id_to_symbol):
+        """
+        Replace symbols in the S-expression based on the given mapping.
+
+        Args:
+            id_to_symbol: A mapping from id(node) to a new symbol.
+        """
+        return SExpression(
+            id_to_symbol.get(id(self), self.symbol),
+            tuple(child.replace_symbols_by_id(id_to_symbol) for child in self.children),
+        )
+
+    def replace_nodes_by_id(self, id_to_new_node):
+        """
+        Replace nodes in the S-expression based on the given mapping. Any remapped
+            node will not have its children replaced.
+
+        Args:
+            id_to_new_node: A mapping from id(node) to a new node.
+        """
+        if id(self) in id_to_new_node:
+            return id_to_new_node[id(self)]
+        return SExpression(
+            self.symbol,
+            tuple(child.replace_nodes_by_id(id_to_new_node) for child in self.children),
+        )
+
 
 @dataclass
 class InitializedSExpression:
