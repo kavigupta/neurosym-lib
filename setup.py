b88f0e1b40e37a43b06fd15ebfd4d1afd187c1c5
Kavi Gupta <kavi@berkeley.edu>
2024-05-31 13:28:29 -0400

    add create smoothing mask (#106)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neurosym",
-    version="0.0.52",
+    version="0.0.53",
     author="Kavi Gupta, Atharva Sehgal, Maddy Bowers, Armando Solar-Lezama",
     author_email="kavig+neurosym@mit.edu",
     description="Neurosymbolic library.",
