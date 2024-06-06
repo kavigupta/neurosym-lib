92a042c662a8524135ec169f829d707ff2d3fe9a
Kavi Gupta <kavi@berkeley.edu>
2024-06-06 18:46:55 -0400

    add tutorial test

--- tests/tutorial/test_tutorial_2.py
+++ tests/tutorial/test_tutorial_2.py
@@ -1,6 +1,7 @@
 import json
 import unittest
 
+from tests.tutorial.utils import execute_notebook
 from tutorial.process_tutorial import create_skeleton
 
 
@@ -12,3 +13,38 @@ class TestTutorial2(unittest.TestCase):
             notebook = json.load(f)
 
         self.assertEqual(create_skeleton(solutions), notebook)
+
+    def test_tutorial_2_runnable(self):
+        result = execute_notebook(
+            "tutorial/tutorial2_discrete_solutions.ipynb",
+            suffix="import json; print('*' * 80); print(json.dumps(abstraction_dsl.render()))",
+        )
+        *_, stars, actual, _ = result.split("\n")
+        self.assertEqual(stars, "*" * 80)
+        actual = json.loads(actual)
+        expected = """
+              0 :: () -> f
+              1 :: () -> f
+              2 :: () -> f
+              + :: (f, f) -> f
+              - :: (f, f) -> f
+              * :: (f, f) -> f
+             ** :: (f, f) -> f
+              / :: (f, f) -> f
+            sin :: f -> f
+           sqrt :: f -> f
+              < :: (f, f) -> b
+            ite :: (b, f, f) -> f
+            lam :: L<#body|f> -> f -> #body
+           $0_0 :: V<f@0>
+           __10 :: (f, f) -> f = (lam-abstr (#0 #1) (sin (/ #1 #0)))
+           __20 :: () -> f = (lam-abstr () (sqrt (2)))
+           __30 :: (f, f) -> f -> f = (lam-abstr (#0 #1) (lam (- #1 (sin (** #0 ($0_0))))))
+           __40 :: (f, f) -> f = (lam-abstr (#0 #1) (ite (< #1 #0) (1) (0)))
+           __50 :: () -> f = (lam-abstr () (sin (1)))
+        """
+        actual_lines, expected_lines = [
+            [line.strip() for line in text.split("\n") if line.strip()]
+            for text in (actual, expected)
+        ]
+        self.assertEqual(actual_lines, expected_lines)
