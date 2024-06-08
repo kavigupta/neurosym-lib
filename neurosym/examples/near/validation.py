ea308713a2b7c75e9ba1beec63b126bc03afdc6a
Kavi Gupta <kavi@berkeley.edu>
2024-06-07 20:44:03 -0400

    Make validation cost a class (#134)

---
+++ neurosym/examples/near/validation.py
@@ -0,0 +1,47 @@
+import torch
+
+from neurosym.examples.near.methods.near_example_trainer import NEARTrainer
+from neurosym.examples.near.models.torch_program_module import TorchProgramModule
+from neurosym.examples.near.neural_dsl import PartialProgramNotFoundError
+from neurosym.search_graph.dsl_search_node import DSLSearchNode
+from neurosym.utils.imports import import_pytorch_lightning
+
+
+class ValidationCost:
+    def __init__(
+        self, *, neural_dsl, trainer_cfg, datamodule, error_loss=10000, **kwargs
+    ):
+        self.neural_dsl = neural_dsl
+        self.trainer_cfg = trainer_cfg
+        self.datamodule = datamodule
+        self.error_loss = error_loss
+        self.kwargs = kwargs
+
+    def __call__(self, node: DSLSearchNode) -> float:
+        pl = import_pytorch_lightning()
+
+        trainer = pl.Trainer(
+            max_epochs=self.trainer_cfg.n_epochs,
+            devices="auto",
+            accelerator="cpu",
+            enable_checkpointing=False,
+            logger=False,
+            **self.kwargs,
+        )
+        try:
+            initialized_p = self.neural_dsl.initialize(node.program)
+        except PartialProgramNotFoundError:
+            return self.error_loss
+
+        model = self.neural_dsl.compute(initialized_p)
+        if not isinstance(model, torch.nn.Module):
+            del model
+            del initialized_p
+            model = TorchProgramModule(dsl=self.neural_dsl, program=node.program)
+        pl_model = NEARTrainer(model, config=self.trainer_cfg)
+        trainer.fit(
+            pl_model,
+            self.datamodule.train_dataloader(),
+            self.datamodule.val_dataloader(),
+        )
+        return trainer.callback_metrics["val_loss"].item()
