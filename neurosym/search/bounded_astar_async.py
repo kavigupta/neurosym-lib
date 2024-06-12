fa73d80bf8ab23391100d343d4c0548134592e53
Atharva Sehgal <atharva.sehgal@gmail.com>
2024-06-12 17:12:10 +0000

    adding async + tests

---
+++ neurosym/search/bounded_astar_async.py
@@ -0,0 +1,124 @@
+# pylint: skip-file
+import os
+import pickle
+import dill
+import queue
+from concurrent.futures import Future, ProcessPoolExecutor
+from typing import Callable
+
+from tqdm.auto import tqdm
+
+from neurosym.programs.s_expression import SExpression
+from neurosym.programs.s_expression_render import render_s_expression
+from neurosym.search_graph.search_graph import SearchGraph
+from neurosym.search.bounded_astar import BoundedAStarNode
+
+
+class FuturePriorityQueue(queue.PriorityQueue):
+    """
+    A priority queue with primitive support for futures.
+    """
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.waiting = []
+
+    def syncronize(self):
+        # check if any of the waiting items are done
+        for i, (item, item_fn) in enumerate(self.waiting):
+            if item.done():
+                # add to the queue
+                self.put(dill.dumps(item_fn(item.result())))
+                # remove from waiting
+                self.waiting.pop(i)
+
+    def putitem(self, item: Future, future_fn: Callable):
+        """Making sure this doesn't override the `put` method."""
+        self.waiting.append((item, future_fn))
+        self.syncronize()
+
+    def empty(self):
+        return len(self.waiting) == 0 and super().empty()
+
+    def getitem(self):
+        """Making sure this doesn't override the `get` method."""
+        self.syncronize()
+        out = self.get_nowait()
+        return dill.loads(out)
+
+
+def bounded_astar_async(
+    g: SearchGraph,
+    cost_plus_heuristic: Callable[[SExpression], float],
+    max_depth: int,
+    max_workers: int,
+    verbose: bool = False,
+):
+    """
+    Performs a bounded a-star search on the given search graph, yielding each node in
+    the order it was visited. Evaluates the cost_plus_heuristic function asynchronously
+    spawning max_workers processes. This function doesn't optimally traverse the search
+    graph.
+
+    :param g: Search graph to search over
+    :param cost_plus_heuristic: Cost plus heuristic function to use for A*.
+        The heuristic function should be admissible, i.e. it should never overestimate
+        the cost to reach the goal.
+    :param max_depth: Maximum depth to search to.
+    :param max_workers: Maximum number of workers to use for evaluating
+        the cost_plus_heuristic function.
+    """
+    assert max_depth > 0, "Cannot have 0 depth."
+    assert max_workers > 0, "Cannot have 0 workers."
+
+    with ProcessPoolExecutor(max_workers) as executor:
+        visited = set()
+        fringe = FuturePriorityQueue()
+
+        def add_to_fringe(node, depth):
+            future = executor.submit(cost_plus_heuristic, node)
+            fringe.putitem(
+                future, future_fn=lambda ret: BoundedAStarNode(ret[0], node, depth, ret[1])
+            )
+
+        add_to_fringe(g.initial_node(), 0)
+        best_node = None
+        if verbose:
+            pbar = tqdm(leave=False)
+        while not fringe.empty():
+            try:
+                fringe_var = fringe.getitem()
+                node, depth = fringe_var.node, fringe_var.depth
+                if node.program in visited or depth > max_depth:
+                    continue
+                visited.add(node.program)
+                if g.is_goal_node(node):
+                    yield node
+                for child in g.expand_node(node):
+                    add_to_fringe(child, depth + 1)
+
+                if best_node is None or fringe_var.cost < best_node.cost:
+                    best_node = fringe_var
+                    # save the best node so far:
+                    if not os.path.exists("list_best_nodes.pkl"):
+                        with open("list_best_nodes.pkl", "wb") as f:
+                            pickle.dump([], f)
+                    
+                    with open("list_best_nodes.pkl", "rb") as f:
+                        best_nodes = pickle.load(f)
+                    best_nodes.append(best_node)
+                    with open("list_best_nodes.pkl", "wb") as f:
+                        pickle.dump(best_nodes, f)
+
+                if verbose:
+                    depth = best_node.depth
+                    cost = best_node.cost
+                    program = render_s_expression(best_node.node.program)
+                    program_acc = best_node.metrics[0]
+                    program_auroc = best_node.metrics[1]
+                    pbar.set_description(
+                        f"Depth: {depth}, Cost: {cost:.4}, Program: {program:.50}, F1: {program_acc:.4}, AUROC: {program_auroc:.4}"
+                    )
+                    pbar.update(1)
+            except queue.Empty:
+                pass
--- tests/near/test_sequential_dsl.py
+++ tests/near/test_sequential_dsl.py
@@ -91,6 +91,79 @@ class TestNEARSequentialDSL(unittest.TestCase):
                 if n_iter > 30:
                     break
 
+    def test_sequential_dsl_astar_async(self):
+        """
+        A minimal implementation of NEAR with a simple DSL.
+        search = A-star
+        heuristic = validation score after training for N epochs. (pl.Trainer)
+        goal = Fully symbolic program. (handled in: search_graph/dsl_search_graph.py)
+        test_predicate = score on testing set (pl.Trainer)
+
+        This test case is similar to the previous one, but tests an async version of
+        bounded_astar search.
+        """
+        datamodule = ns.datasets.near_data_example(train_seed=0)
+        input_dim, output_dim = datamodule.train.get_io_dims()
+        original_dsl = near.example_rnn_dsl(input_dim, output_dim)
+        trainer_cfg = near.NEARTrainerConfig(
+            max_seq_len=100,
+            n_epochs=10,
+            num_labels=output_dim,
+            train_steps=len(datamodule.train),
+        )
+        t = ns.TypeDefiner(L=input_dim, O=output_dim)
+        t.typedef("fL", "{f, $L}")
+        t.typedef("fO", "{f, $O}")
+        neural_dsl = near.NeuralDSL.from_dsl(
+            dsl=original_dsl,
+            modules={
+                **near.create_modules(
+                    "mlp",
+                    [t("($fL) -> $fL"), t("($fL) -> $fO")],
+                    near.mlp_factory(hidden_size=10),
+                ),
+                **near.create_modules(
+                    "rnn_seq2seq",
+                    [t("([$fL]) -> [$fL]"), t("([$fL]) -> [$fO]")],
+                    near.rnn_factory_seq2seq(hidden_size=10),
+                ),
+                **near.create_modules(
+                    "rnn_seq2class",
+                    [t("([$fL]) -> $fL"), t("([$fL]) -> $fO")],
+                    near.rnn_factory_seq2class(hidden_size=10),
+                ),
+            },
+        )
+
+        g = near.near_graph(
+            neural_dsl,
+            ns.parse_type(
+                s="([{f, $L}]) -> [{f, $O}]",
+                env=ns.TypeDefiner(L=input_dim, O=output_dim),
+            ),
+            is_goal=neural_dsl.program_has_no_holes,
+        )
+        # succeed if this raises StopIteration
+        with pytest.raises(StopIteration):
+            n_iter = 0
+            iterator = ns.search.bounded_astar_async(
+                g,
+                ValidationCost(
+                    neural_dsl=neural_dsl,
+                    trainer_cfg=trainer_cfg,
+                    datamodule=datamodule,
+                ),
+                max_depth=3,
+                max_workers=4,
+            )
+            while True:
+                print("iteration: ", n_iter)
+                n_iter += 1
+                node = next(iterator)
+                self.assertIsNotNone(node)
+                if n_iter > 30:
+                    break
+
     def test_sequential_dsl_enumerate(self):
         """
         Enumerate all programs in dsl upto fixed depth. This test case makes
