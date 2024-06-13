c5dcc4e228065079efe510b555b2ee11e5d75ad7
Kavi Gupta <kavi@berkeley.edu>
2024-06-13 19:38:43 -0400

    update

--- tests/file_checks_tests.py
+++ tests/file_checks_tests.py
@@ -5,9 +5,12 @@ import os
 import re
 import unittest
 from functools import lru_cache
+from types import ModuleType
 
 import parameterized
 
+import neurosym as ns
+from neurosym.examples import near
 from tests.tutorial.utils import ipynb_to_py
 
 
@@ -117,7 +120,7 @@ class AllFunctionsDocumentedTest(unittest.TestCase):
     @parameterized.parameterized.expand(
         [(path,) for path in files_to_examine("tests", "tutorial")]
     )
-    def test_all_functions_dcoumented(self, path):
+    def test_all_used_functions_documented(self, path):
         code = read_python_file(path)
         functions = set(x.group() for x in self.pattern.finditer(code))
         functions = {self.normalize(f) for f in functions}
@@ -126,3 +129,18 @@ class AllFunctionsDocumentedTest(unittest.TestCase):
         if not extras:
             return
         self.fail(f"Found {len(extras)} undocumented functions: {extras}")
+
+    def assertFieldDocumented(self, name, base_module, base_module_name):
+        if name.startswith("_"):
+            return
+        if isinstance(getattr(base_module, name), ModuleType):
+            return
+        self.assertIn(base_module_name + "." + name, documented_functions())
+
+    @parameterized.parameterized.expand([(name,) for name in dir(ns)])
+    def test_ns_function_documented(self, name):
+        self.assertFieldDocumented(name, ns, "neurosym")
+
+    @parameterized.parameterized.expand([(name,) for name in dir(near)])
+    def test_near_function_documented(self, name):
+        self.assertFieldDocumented(name, near, "neurosym.examples.near")
