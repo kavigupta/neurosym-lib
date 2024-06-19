7e736a10dd9f4a7db307b2d695ddd94d65523824
Kavi Gupta <kavi@berkeley.edu>
2024-06-19 19:34:04 -0400

    update

--- neurosym/python_dsl/python_def_use_mask/__init__.py
+++ neurosym/python_dsl/python_def_use_mask/__init__.py
@@ -14,4 +14,4 @@ from .mask import DefUseChainPreorderMask, DefUseMaskConfiguration
 from .names import GLOBAL_REGEX, NAME_REGEX, match_either_name_or_global
 from .ordering import PythonNodeOrdering, python_ordering_dictionary
 from .special_case_symbol_predicate import SpecialCaseSymbolPredicate
-from .target_handler import TargetHandler
+from .target_handler import TargetHandler, create_target_handler
