e9417cb02888e7189a8a1d2e54fbf7645fff475f
Kavi Gupta <kavi@berkeley.edu>
2024-06-19 18:24:54 -0400

    document handlers for python def use mask

--- docs/source/python_def_use_mask.rst
+++ docs/source/python_def_use_mask.rst
@@ -7,3 +7,10 @@ Python Def-Use Chain Preorder Mask
     :members:
 
 .. autofunction:: neurosym.python_def_use_mask.match_either_name_or_global
+
+Handlers for Python Def-Use Chain Preorder Mask
+------------------------------------------------
+
+.. autoclass:: neurosym.python_def_use_mask.Handler
+    :members:
+.. autoclass:: neurosym.python_def_use_mask.ConstructHandler
--- neurosym/python_dsl/python_def_use_mask/handler.py
+++ neurosym/python_dsl/python_def_use_mask/handler.py
@@ -9,8 +9,21 @@ from .special_case_symbol_predicate import SpecialCaseSymbolPredicate
 
 class Handler(ABC):
     """
-    Corresponds to a given node in the SExpression AST. Keeps track of a list of defined
-        production indices, in order of definition.
+    Corresponds to a given node in the ``PythonAST``. Keeps track of a list of defined
+    production indices, in order of definition, along with a reference to the mask, and
+    the configuration.
+
+    The handler is responsible for determining whether a given context is defining or not,
+    and for computing the mask for a given set of names.
+
+    The handler is also responsible for determining the handler for a given child, and for
+    performing tasks related to the child when it is exited.
+
+    :param mask: The ``DefUsePreorderMask`` that is being used, this is a circular reference
+        used for convenience.
+    :param defined_production_idxs: The list of defined production indices, in order of
+        definition.
+    :param config: The configuration for the mask.
     """
 
     def __init__(self, mask, defined_production_idxs, config):
@@ -23,18 +36,20 @@ class Handler(ABC):
         self, position: int, symbol: int
     ) -> Tuple["Handler", Callable[[], None]]:
         """
-        When a child is entered, this method is called to determine the handler.
+        When a child is entered, this method is called to determine the handler for the child.
+        This method can also perform tasks related to entering the child. By default, this
+        method returns the default handler for the child, and does nothing else.
 
-        Args:
-            position: The position in the s-expression.
-            symbol: The symbol of the child (index into a grammar's symbols list)
-                Note: this can include variables, e.g., const-&x:0-Name,
-                   but it can also include production symbols like Assign~S
-                   or other leaves like const-i2~Const.
+        This method can be undone by calling the returned function.
 
-        Returns:
-            The handler for the child.
-            A function that can be called to undo the changes made by this function.
+        :param position: The position in the s-expression.
+        :param symbol: The symbol of the child (index into a grammar's symbols list).
+            Note: this can include variables, e.g., const-&x:0-Name,
+            but it can also include production symbols like Assign~S
+            or other leaves like const-i2~Const.
+
+        :return: ``(child_handler, undo)``: The handler for the child and a function
+            that can be called to undo the changes made by this function.
         """
         return (
             default_handler(
@@ -51,18 +66,23 @@ class Handler(ABC):
         self, position: int, symbol: int, child: "Handler"
     ) -> Callable[[], None]:
         """
-        When a child is exited, this method is called to perform tasks related
-            to the child.
+        When a child is exited, this method is called to perform tasks related to
+        processing the information from the child. By default, this method does nothing.
 
-        Returns:
-            A function that can be called to undo the changes made by this function.
+        :param position: The position in the s-expression.
+        :param symbol: The symbol of the child. See ``on_child_enter`` for more information.
+        :param child: The handler for the child.
+
+        :return: A function that can be called to undo the changes made by this function.
         """
         del position, symbol, child
         return lambda: None
 
     def currently_defined_indices(self) -> list[int]:
         """
-        Returns the list of currently defined symbols, in order of definition.
+        Returns the list of currently defined symbols, in order of definition. This is
+        important because we need to know which symbols are currently defined for some
+        purposes, e.g., when using imperative de bruijn indices.
         """
         assert isinstance(self.defined_production_idxs, list)
         return self.defined_production_idxs
@@ -73,16 +93,18 @@ class Handler(ABC):
         Returns whether the context at the given position is defining.
 
         E.g., for an Assign node, the left-hand side is defining, and
-            the right-hand side is not. This is important because for
-            defining contexts, we do not need to use a previously
-            defined variable.
+        the right-hand side is not. This is important because for
+        defining contexts, we do not need to use a previously
+        defined variable, so new variables will not be masked out.
+
+        :param position: The position in the s-expression.
         """
 
     def currently_defined_names(self):
         """
         Return the set of currently defined names. Note that this
-            isn't the set of production symbols like const-&x:0-Name,
-            but rather a set of names like x.
+        isn't the set of production symbols like ``const-&x:0-Name``,
+        but rather a set of names like ``x``.
         """
         names = set()
         for symbol in self.currently_defined_indices():
@@ -97,7 +119,12 @@ class Handler(ABC):
         self, position: int, symbol: int
     ) -> Tuple["Handler", Callable[[], None]]:
         """
-        Return a handler collecting targets for the given child.
+        Return a handler for a target. This is used when we are processing a target,
+        e.g., the left-hand side of an assignment. By default, this method returns the
+        default handler for the child, and does nothing else.
+
+        :param position: The position in the s-expression.
+        :param symbol: The symbol of the child. See ``on_child_enter`` for more information.
         """
         # pylint: disable=cyclic-import
         from .target_handler import create_target_handler
@@ -134,8 +161,18 @@ class Handler(ABC):
     ):
         """
         Compute the mask for the given names. If the context is defining,
-            then all symbols are valid. Otherwise, only the symbols that
-            match the names are valid.
+        then all symbols are valid. Otherwise, only the symbols that
+        match the names are valid.
+
+        :param position: The position in the s-expression.
+        :param symbols: The list of symbols to compute the mask for.
+        :param idx_to_name: The list of names for the symbols.
+        :param special_case_predicates: The list of special case predicates to use.
+            A special case predicate is a way to extend the behavior of the handler
+            by providing a custom predicate for a given symbol as to whether
+            it should be considered valid or not.
+
+        :return: A list of booleans, where ``True`` indicates that the symbol is valid.
         """
         assert isinstance(special_case_predicates, list) and all(
             isinstance(x, SpecialCaseSymbolPredicate) for x in special_case_predicates
@@ -151,7 +188,10 @@ class Handler(ABC):
 
 class ConstructHandler(Handler):
     """
-    Handler for a single construct.
+    Base class for Handlers that correspond to a specific construct in the AST.
+
+    :field name: The name of the construct. This must be the name of a subclass of
+        ``ast.AST``. This is used to determine the child fields of the construct.
     """
 
     # must be overridden in subclasses, represents the name of the construct
