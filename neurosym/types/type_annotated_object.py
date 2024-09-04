2c81c021901c7e139d0c817556bac3684eb8bbd0
Kavi Gupta <kavi@berkeley.edu>
2024-09-04 17:04:11 -0400

    add documentation

--- neurosym/types/type_annotated_object.py
+++ neurosym/types/type_annotated_object.py
@@ -6,7 +6,11 @@ from .type import Type
 @dataclass
 class TypeAnnotatedObject:
     """
-    An object that has a type annotation.
+    An object that has a type annotation. This is used to pass around objects
+    in environments, with their types.
+
+    :param object_type: The type of the object.
+    :param object_value: The object itself.
     """
 
     object_type: Type
