61e2e3cdbfe9a3a2f3b71e16e6d573f715ccb160
Kavi Gupta <kavi@berkeley.edu>
2024-10-30 18:36:28 -0400

    add ability to bind a graph

--- neurosym/search_graph/bind_search_graph.py
+++ neurosym/search_graph/bind_search_graph.py
@@ -3,28 +3,48 @@ from typing import Callable, Generic, Iterable, TypeVar, Union
 import uuid
 
 from neurosym.search_graph.search_graph import SearchGraph
+from neurosym.utils.documentation import internal_only
 
 A = TypeVar("A")
 B = TypeVar("B")
 
 
 @dataclass(frozen=True)
+@internal_only
 class BindSearchGraphNodeA(Generic[A]):
+    """
+    Represents a node in the initial search graph.
+
+    node: The node in the initial search graph.
+    """
+
     node: A
 
 
 @dataclass(frozen=True, unsafe_hash=True)
+@internal_only
 class BindSearchGraphNodeB(Generic[B]):
+    """
+    Represents a node in the bound search graph (the second one).
+
+    :param node: The node in the second search graph.
+    :param graph_b: The second search graph. This is stored here for garbage collection purposes,
+    but it could be stored in BindSearchGraph itself. However, that would lead to memory leaks if
+    that particular part of the graph is not reachable from the root node. This is why we store it
+    here and do not use it for comparison or hashing.
+    :param graph_b_uuid: The UUID of the second search graph. This is used to compare nodes in the
+    search graph as well as to hash them.
+    """
+
     node: B = field(compare=True, hash=True)
-    # reference is stored here for gc purposes. it could be stored in BindSearchGraph
-    # but that would lead to memory leaks if that particular part of the graph is not
-    # reachable from the root node.
     graph_b: SearchGraph[B] = field(compare=False, hash=False)
-    # uuid is used to identify the graph_b instance. It is used to compare nodes
-    # in the search graph as well as to hash them.
     graph_b_uuid: uuid.UUID = field(compare=True, hash=True)
 
+    @internal_only
     def with_node(self, node: B) -> "BindSearchGraphNodeB[B]":
+        """
+        Returns a new node with the given node.
+        """
         return BindSearchGraphNodeB(node, self.graph_b, self.graph_b_uuid)
 
 
@@ -32,6 +52,15 @@ BindSearchGraphNode = Union[BindSearchGraphNodeA[A], BindSearchGraphNodeB[B]]
 
 
 class BindSearchGraph(SearchGraph[BindSearchGraphNode[A, B]]):
+    """
+    Combines two search graphs into one. The first search graph's nodes spawn the second search
+    graph.
+
+    This is a monad-like operation that allows us to bind two search graphs together. Unlike
+    a monadic bind, the overall graph does not have the type of the second graph's nodes. Instead,
+    you need to manually unpack the second graph's nodes.
+    """
+
     def __init__(
         self, graph_a: SearchGraph[A], create_graph_b: Callable[[A], SearchGraph[B]]
     ):
@@ -55,8 +84,7 @@ class BindSearchGraph(SearchGraph[BindSearchGraphNode[A, B]]):
                 for neighbor_a in self.graph_a.expand_node(node.node):
                     yield BindSearchGraphNodeA(neighbor_a)
         else:
-            graph_b = self.graph_b(node)
-            for neighbor_b in graph_b.expand_node(node.node):
+            for neighbor_b in node.graph_b.expand_node(node.node):
                 yield node.with_node(neighbor_b)
 
     def is_goal_node(self, node: BindSearchGraphNode[A, B]) -> bool:
--- tests/basic_search/bind_test.py
+++ tests/basic_search/bind_test.py
@@ -70,3 +70,21 @@ class TestSearch(unittest.TestCase):
         node = next(ns.search.astar(g)).node
         # capable of backtracking slightly to find the optimal solution
         self.assertEqual(node, "2B2B2B2B")
+
+    def test_bind_heterogenous_type(self):
+        g = ns.BindSearchGraph(
+            StringContentsSearchGraph(
+                start="",
+                moves=["1A", "1B", "22A", "2B"],
+                character_to_match="2",
+                desired_number=4,
+            ),
+            lambda x: StringContentsSearchGraph(
+                start=(x,),
+                moves=[("1",), ("2",)],
+                character_to_match="2",
+                desired_number=4,
+            ),
+        )
+        node = next(ns.search.astar(g)).node
+        self.assertEqual(node, ("22A22A", "2", "2", "2", "2"))
