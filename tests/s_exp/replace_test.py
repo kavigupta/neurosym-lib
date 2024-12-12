39920c40cb8ee7f9ca378f6fc23629e4e3741b73
Kavi Gupta <kavi@berkeley.edu>
2024-12-11 23:41:33 -0500

    add ability to replace the first element in a sequence (#267)

--- tests/s_exp/replace_test.py
+++ tests/s_exp/replace_test.py
@@ -37,3 +37,45 @@ class TestReplaceNode(unittest.TestCase):
         }
         replaced = s_exp.replace_nodes_by_id(id_to_node)
         self.assertEqual(ns.render_s_expression(replaced), "(abc (def (ghi)))")
+
+
+class TestReplaceFirst(unittest.TestCase):
+    def compute_dsl(self):
+        dslf = ns.DSLFactory()
+        dslf.concrete("1", "() -> i", lambda: 1)
+
+        def counter_fn(counter):
+            counter[0] += 1
+            return counter[0]
+
+        dslf.parameterized("c", "() -> i", counter_fn, dict(counter=lambda: [0]))
+        dslf.parameterized(
+            "+",
+            "(i, i) -> i",
+            lambda x, y, counter: x + y + counter_fn(counter),
+            dict(counter=lambda: [0]),
+        )
+        dsl = dslf.finalize()
+        return dsl
+
+    def test_replace_one_of_multiple(self):
+        dsl = self.compute_dsl()
+        a = dsl.initialize(ns.parse_s_expression("(+ (+ (1) (c)) (c))"))
+        a_states = list(a.all_state_values())
+        b = dsl.initialize(ns.parse_s_expression("(c)"))
+        c, replaced = a.replace_first("c", b)
+        self.assertTrue(replaced)
+        self.assertEqual(
+            ns.render_s_expression(c.uninitialize()), "(+ (+ (1) (c)) (c))"
+        )
+        # exact copy of the right child
+        self.assertIs(a.children[1], c.children[1])
+        # exact copy of the left child of the left child
+        self.assertIs(a.children[0].children[0], c.children[0].children[0])
+        # b placed in
+        self.assertIs(b, c.children[0].children[1])
+        # a is not mutated.
+        a_states_new = list(a.all_state_values())
+        self.assertEqual(len(a_states), len(a_states_new))
+        for old, new in zip(a_states, a_states_new):
+            self.assertIs(old, new)
