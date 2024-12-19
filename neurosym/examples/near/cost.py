00a1c9bda1ff6e265a73a5648202dc4bea5a5f53
Kavi Gupta <kavi@berkeley.edu>
2024-12-19 15:51:12 -0500

    update

---
+++ neurosym/examples/near/cost.py
@@ -0,0 +1,89 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Tuple
+
+from neurosym.programs.hole import Hole
+from neurosym.programs.s_expression import (
+    InitializedSExpression,
+    SExpression,
+    is_initialized_s_expression,
+)
+from neurosym.search_graph.dsl_search_node import DSLSearchNode
+
+
+class StructuralCost(ABC):
+    """
+    Compute a structural cost for a program. This is used to guide the search towards
+    more completed programs.
+    """
+
+    @abstractmethod
+    def compute_structural_cost(self, model: SExpression) -> float:
+        """
+        Computes the structural cost of a partial program.
+        """
+
+
+class NearValidationHeuristic(ABC):
+    """
+    Compute a validation cost for a program. This is used to guide the search towards
+    more accurate programs.
+    """
+
+    @abstractmethod
+    def train_and_compute_cost(
+        self, model: InitializedSExpression
+    ) -> Tuple[InitializedSExpression, float]:
+        """
+        Train a model and compute the validation cost. This is allowed to
+        mutate the original model.
+        """
+
+    def compute_cost(self, model: InitializedSExpression) -> float:
+        """
+        Compute the validation cost of a model.
+        """
+        _, val_loss = self.train_and_compute_cost(model)
+        return val_loss
+
+
+@dataclass
+class NearCost:
+    """
+    Near cost function that combines a structural cost and a validation heuristic.
+    """
+
+    structural_cost: StructuralCost
+    validation_heuristic: NearValidationHeuristic
+    structural_cost_weight: float = 0.5
+
+    def compute_cost(self, model: InitializedSExpression) -> float:
+        """
+        Compute the cost of a model.
+        """
+        val_loss = self.validation_heuristic.compute_cost(model)
+        struct_cost = self.structural_cost.compute_structural_cost(model.uninitialize())
+        return (
+            1 - self.structural_cost_weight
+        ) * val_loss + self.structural_cost_weight * struct_cost
+
+    def __call__(self, node: DSLSearchNode) -> float:
+        """
+        Compute the cost of a model.
+        """
+        program = node.program
+        if not isinstance(program, InitializedSExpression):
+            program = node.dsl.initialize(program)
+        assert is_initialized_s_expression(program), type(program)
+        return self.compute_cost(program)
+
+
+class NumberHolesStructuralCost(StructuralCost):
+    def compute_structural_cost(self, model: SExpression) -> float:
+        cost = 0
+        if isinstance(model, Hole):
+            cost += 1
+            return cost
+        for child in model.children:
+            cost += self.compute_structural_cost(child)
+        return cost
