e0cf7b8392ace8b8d4b2eff4a493ba6b2f15b18b
Kavi Gupta <kavi@berkeley.edu>
2025-05-14 22:49:30 -0400

    Allow sexpression with non str head (#319)

--- neurosym/programs/s_expression_render.py
+++ neurosym/programs/s_expression_render.py
@@ -5,7 +5,9 @@ from s_expression_parser import Pair, ParserConfig, Renderer, nil, parse
 from neurosym.programs.s_expression import SExpression
 
 
-def _to_pair(s_exp: SExpression, *, for_stitch: bool) -> Pair:
+def _to_pair(
+    s_exp: SExpression, *, for_stitch: bool, allow_sexp_head: bool = False
+) -> Pair:
     """
     Convert an SExpression to a Pair.
 
@@ -24,13 +26,23 @@ def _to_pair(s_exp: SExpression, *, for_stitch: bool) -> Pair:
     if isinstance(s_exp, str):
         return s_exp
     assert hasattr(s_exp, "children"), f"Expected SExpression, got {s_exp}"
+    symbol = s_exp.symbol
+    if isinstance(symbol, SExpression):
+        if not allow_sexp_head:
+            raise ValueError(
+                "SExpression head is not allowed in this context. "
+                "Set allow_sexp_head=True to allow it."
+            )
+        symbol = _to_pair(symbol, for_stitch=for_stitch, allow_sexp_head=True)
     if for_stitch and not s_exp.children:
-        if s_exp.symbol.startswith("$"):
-            return s_exp.symbol
-        return "leaf-" + s_exp.symbol
-    elements = [s_exp.symbol] + [
-        _to_pair(x, for_stitch=for_stitch) for x in s_exp.children
-    ]
+        if not isinstance(symbol, str):
+            raise ValueError(
+                "Expected string symbol for leaf, got {symbol} of type {type(symbol)}"
+            )
+        if symbol.startswith("$"):
+            return symbol
+        return "leaf-" + symbol
+    elements = [symbol] + [_to_pair(x, for_stitch=for_stitch) for x in s_exp.children]
     result = nil
     for element in reversed(elements):
         result = Pair(element, result)
@@ -38,7 +50,10 @@ def _to_pair(s_exp: SExpression, *, for_stitch: bool) -> Pair:
 
 
 def _from_pair(
-    pair: Pair, should_not_be_leaf: Set[str], for_stitch: bool
+    pair: Pair,
+    should_not_be_leaf: Set[str],
+    for_stitch: bool,
+    allow_sexp_head: bool,
 ) -> SExpression:
     """
     Convert a Pair to an SExpression.
@@ -48,6 +63,9 @@ def _from_pair(
         should_not_be_leaf: A set of symbols that should not be converted to leaves.
             Instead, they should be converted to SExpressions with no children.
         for_stitch: Whether the Pair is being converted for use with stitch.
+        allow_sexp_head: Whether we are allowing the head of the SExpression
+            to be an SExpression. This is useful for parsing the head of a program
+            as an SExpression, which is not standard.
 
     Returns:
         The SExpression representing the Pair.
@@ -66,16 +84,31 @@ def _from_pair(
     while pair is not nil:
         car = pair.car
         if not elements:
-            assert isinstance(car, str), f"Expected string, got {pair.car}"
+            if allow_sexp_head:
+                car = _from_pair(
+                    car,
+                    should_not_be_leaf=should_not_be_leaf,
+                    for_stitch=for_stitch,
+                    allow_sexp_head=allow_sexp_head,
+                )
+            else:
+                assert isinstance(car, str), f"Expected string, got {pair.car}"
         else:
-            car = _from_pair(car, should_not_be_leaf, for_stitch=for_stitch)
+            car = _from_pair(
+                car,
+                should_not_be_leaf,
+                for_stitch=for_stitch,
+                allow_sexp_head=allow_sexp_head,
+            )
         elements.append(car)
         pair = pair.cdr
     head, *tail = elements
     return SExpression(head, tuple(tail))
 
 
-def render_s_expression(s_exp: SExpression, for_stitch: bool = False) -> str:
+def render_s_expression(
+    s_exp: SExpression, for_stitch: bool = False, allow_sexp_head: bool = False
+) -> str:
     """
     Render an SExpression as a string.
 
@@ -83,15 +116,23 @@ def render_s_expression(s_exp: SExpression, for_stitch: bool = False) -> str:
     :param for_stitch: Whether we are parsing this expression from ``stitch_core``,
         in which case we need to handle the "leaf-" prefix that we introduced
         in ``parse_s_expression``.
-
+    :param allow_sexp_head: Whether we are allowing the head of the SExpression
+        to be an SExpression. This is useful for parsing the head of a program
+        as an SExpression, which is not standard.
 
     :returns: The string representing the SExpression.
     """
-    return Renderer(columns=float("inf")).render(_to_pair(s_exp, for_stitch=for_stitch))
+    return Renderer(columns=float("inf")).render(
+        _to_pair(s_exp, for_stitch=for_stitch, allow_sexp_head=allow_sexp_head)
+    )
 
 
 def parse_s_expression(
-    s: str, *, should_not_be_leaf: Set[str] = None, for_stitch: bool = False
+    s: str,
+    *,
+    should_not_be_leaf: Set[str] = None,
+    for_stitch: bool = False,
+    allow_sexp_head: bool = False,
 ) -> SExpression:
     """
     Parse an SExpression from a string.
@@ -101,6 +142,9 @@ def parse_s_expression(
         Instead, they should be converted to SExpressions with no children.
     :param for_stitch: Whether we are rendering this s-expression for ``stitch_core``,
         in which case we need to tag leaf nodes with "leaf-" prefix.
+    :param allow_sexp_head: Whether we are allowing the head of the SExpression
+        to be an SExpression. This is useful for parsing the head of a program
+        as an SExpression, which is not standard.
 
     :returns: The SExpression representing the string.
     """
@@ -110,7 +154,10 @@ def parse_s_expression(
     if len(pairs) != 1:
         raise ValueError(f"Expected one expression, got {len(pairs)}")
     return _from_pair(
-        pairs[0], should_not_be_leaf=should_not_be_leaf, for_stitch=for_stitch
+        pairs[0],
+        should_not_be_leaf=should_not_be_leaf,
+        for_stitch=for_stitch,
+        allow_sexp_head=allow_sexp_head,
     )
 
 
