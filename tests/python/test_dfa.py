0ac80782ef46ea366a1a900c0739f86df3ca825f
Kavi Gupta <kavi@berkeley.edu>
2025-06-24 00:46:10 -0400

    bump the ast scope package (#321)

--- tests/python/test_dfa.py
+++ tests/python/test_dfa.py
@@ -1,11 +1,27 @@
 import json
+import sys
 import unittest
 
 import neurosym as ns
 
+refresh = False
+
 
 class TestDFARegression(unittest.TestCase):
     def test_python_dfa(self):
-        with open("test_data/dfa.json") as f:
-            dfa = json.load(f)
-        self.assertEqual(ns.python_dfa(), dfa)
+        if sys.version_info < (3, 12):
+            self.skipTest("Python version is below 3.12, skipping DFA test.")
+        if refresh:
+            with open("test_data/dfa.json", "w") as f:
+                json.dump(ns.python_dfa(), f, indent=2)
+        else:
+            with open("test_data/dfa.json") as f:
+                dfa = json.load(f)
+            self.assertEqual(
+                ns.python_dfa(),
+                dfa,
+                "The DFA does not match the expected output. Set refresh=True to update the test data.",
+            )
+
+    def test_refresh(self):
+        self.assertTrue(not refresh, "You left it on refresh=True.")
