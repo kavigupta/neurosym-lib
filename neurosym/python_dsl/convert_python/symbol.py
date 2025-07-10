d13d3e42264b757e9e88d23147e046d06cbb5616
Kavi Gupta <kavi@berkeley.edu>
2025-07-10 15:17:50 -0400

    imports should not be symbols (#325)

--- neurosym/python_dsl/convert_python/symbol.py
+++ neurosym/python_dsl/convert_python/symbol.py
@@ -29,10 +29,15 @@ class PythonSymbol:
         scope = self.scope
         if scope is None:
             return
-        if isinstance(scope, str):
-            assert scope.isdigit(), f"scope must be int or None, got {scope!r}"
-        else:
-            assert isinstance(scope, int), f"scope must be int or None, got {scope!r}"
+        valid = (
+            isinstance(scope, str)
+            and scope.isdigit()
+            or isinstance(scope, (int, _nonsymbol_scope_id))
+        )
+        assert valid, (
+            f"Invalid scope {scope} for symbol {self.name}. "
+            "Scope should be an int, str that is a digit, or import_scope_id."
+        )
 
     @classmethod
     def parse(cls, x):
@@ -44,16 +49,44 @@ class PythonSymbol:
             return cls(name, scope)
         if x.startswith("g"):
             assert x.startswith("g_")
-            return cls(x[2:], None)
+            x = x[2:]
+            if ":" in x:
+                name, scope = x.split(":")
+                return cls(name, _nonsymbol_scope_id(int(scope)))
+            return cls(x, _nonsymbol_scope_id(None))
         return None
 
     def render_symbol(self):
         """
         Render this symbol with scope information.
         """
-        if self.scope is None:
-            return f"g_{self.name}"
+        if isinstance(self.scope, _nonsymbol_scope_id):
+            return f"g_{self.name}{self.scope.render()}"
         return f"&{self.name}:{self.scope}"
+
+
+@dataclass(frozen=True)
+class _nonsymbol_scope_id:
+    scope: Union[int, None]
+
+    def __post_init__(self):
+        assert self.scope is None or isinstance(self.scope, int)
+
+    @classmethod
+    @internal_only
+    def wrap(cls, scope):
+        if isinstance(scope, cls):
+            return scope
+        return cls(scope)
+
+    @internal_only
+    def render(self):
+        """
+        Render this scope id.
+        """
+        if self.scope is None:
+            return ""
+        return f":{self.scope}"
 
 
 @internal_only
@@ -74,11 +107,27 @@ def create_descoper(code):
     for node in ast.walk(code):
         if node in annot:
             if node in globs:
-                results[node] = None
+                results[node] = _nonsymbol_scope_id(None)
                 continue
             if annot[node] not in scopes:
                 scopes.append(annot[node])
             results[node] = scopes.index(annot[node])
+    # anything that's imported should not be a symbol. This isn't great
+    # because it means that import os as x doesn't have x be the symbol,
+    # but it is a good first approximation.
+    node_to_id_scope = {
+        node: (idx, getattr(node, python_ast_tools.name_field(node)))
+        for node, idx in results.items()
+    }
+    import_node_ids = {
+        idx_scope
+        for node, idx_scope in node_to_id_scope.items()
+        if isinstance(node, ast.alias)
+    }
+    # Make the changes to all nodes that are the same as one that is imported
+    for node, idx_scope in node_to_id_scope.items():
+        if idx_scope in import_node_ids:
+            results[node] = _nonsymbol_scope_id.wrap(results[node])
     return results
 
 
