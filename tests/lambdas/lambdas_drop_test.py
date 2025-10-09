747050dd1058de23f8977ba7a2a6bde00e45a58c
Kavi Gupta <kavi@berkeley.edu>
2025-10-08 23:15:14 -0400

    update

--- tests/lambdas/lambdas_drop_test.py
+++ tests/lambdas/lambdas_drop_test.py
@@ -35,3 +35,29 @@ class TestEvaluateDrops(unittest.TestCase):
         self.assertEquivalence("(lam ($1_1))", "(lam (drop0_3 ($0_0)))", dsl)
         self.assertEquivalence("(lam ($2_1))", "(lam (drop0_3 ($1_0)))", dsl)
         self.assertEquivalence("(lam ($1_1))", "(lam (drop2_3 ($1_0)))", dsl)
+
+
+class TestSearchGraphDrops(unittest.TestCase):
+    def test_search_graph_with_drops(self):
+        dsl = basic_drop_dsl()
+        sg = ns.DSLSearchGraph(
+            dsl,
+            hole_set_chooser=ns.ChooseAll(),
+            test_predicate=lambda node: True,
+            target_type=ns.parse_type("(f, f, f) -> f"),
+            metadata_computer=ns.NoMetadataComputer(),
+        )
+        path = ["??::<(f, f, f) -> f>", "(lam ??::<f|0=f,1=f,2=f>)"]
+        node = sg.initial_node()
+        self.assertEqual(path.pop(0), ns.render_s_expression(node.program))
+        while path:
+            target = path.pop(0)
+            print("Expanding to find:", target)
+            nodes = list(sg.expand_node(node))
+            print([ns.render_s_expression(node.program) for node in nodes])
+            self.assertIn(
+                target, [ns.render_s_expression(node.program) for node in nodes]
+            )
+            [node] = [n for n in nodes if ns.render_s_expression(n.program) == target]
+        print("Final node:", ns.render_s_expression(node.program))
+        1 / 0
