a914f4905a268ac62c93f6fd5d9b289a1258fd6d
Kavi Gupta <kavi@berkeley.edu>
2025-10-15 13:22:40 -0400

    fix lint

--- neurosym/examples/near/models/generic_mlp_rnn.py
+++ neurosym/examples/near/models/generic_mlp_rnn.py
@@ -10,7 +10,6 @@ from neurosym.examples.near.neural_hole_filler import NeuralHoleFiller
 from neurosym.types.type import ArrowType, ListType, TensorType
 from neurosym.types.type_annotated_object import TypeAnnotatedObject
 from neurosym.types.type_shape import infer_output_shape
-from neurosym.types.type_string_repr import render_type
 from neurosym.types.type_with_environment import TypeWithEnvironment
 
 
