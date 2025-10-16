2e865a9ba8f9522e3e4b143c52aed296059a6c3e
Kavi Gupta <kavi@berkeley.edu>
2025-10-16 16:49:57 -0400

    update

--- tests/templates/expansion_test.py
+++ tests/templates/expansion_test.py
@@ -369,3 +369,137 @@ class TestDSLExpand(unittest.TestCase):
                 + :: %num -> %num -> %num
             """,
         )
+
+    def test_expand_with_lambdas(self):
+        dslf = ns.DSLFactory()
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("id", "#a -> #a", lambda x: x)
+        dslf.lambdas()
+        dslf.prune_to("i -> i")
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<i@0>
+                1 :: () -> i
+                id :: #a -> #a
+                lam :: L<#body|i> -> i -> #body
+            """,
+        )
+
+    def test_expand_with_lambdas_no_prune(self):
+        dslf = ns.DSLFactory()
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("id", "#a -> #a", lambda x: x)
+        dslf.lambdas()
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<() -> i@0>
+                $0_1 :: V<i -> i@0>
+                $0_2 :: V<i@0>
+                $1_0 :: V<() -> i@1>
+                $1_1 :: V<i -> i@1>
+                $1_2 :: V<i@1>
+                $2_0 :: V<() -> i@2>
+                $2_1 :: V<i -> i@2>
+                $2_2 :: V<i@2>
+                $3_0 :: V<() -> i@3>
+                $3_1 :: V<i -> i@3>
+                $3_2 :: V<i@3>
+                1 :: () -> i
+                id :: #a -> #a
+                lam_0 :: L<#body|> -> () -> #body
+                lam_1 :: L<#body|() -> i> -> (() -> i) -> #body
+                lam_2 :: L<#body|i -> i> -> (i -> i) -> #body
+                lam_3 :: L<#body|i> -> i -> #body
+            """,
+        )
+
+    def test_expand_with_lambdas_in_not_top_level(self):
+        dslf = ns.DSLFactory()
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("id", "((i, #a) -> #a) -> i", lambda x, y: x)
+        dslf.lambdas()
+        dslf.prune_to("i -> i")
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<() -> i@0>
+                $0_1 :: V<i -> i@0>
+                $0_2 :: V<i@0>
+                $1_2 :: V<i@1>
+                $2_2 :: V<i@2>
+                $3_2 :: V<i@3>
+                1 :: () -> i
+                id_0 :: ((i, () -> i) -> () -> i) -> i
+                id_1 :: ((i, i -> i) -> i -> i) -> i
+                id_2 :: ((i, i) -> i) -> i
+                lam_0 :: L<#body|> -> () -> #body
+                lam_1 :: L<#body|i;() -> i> -> (i, () -> i) -> #body
+                lam_2 :: L<#body|i;i -> i> -> (i, i -> i) -> #body
+                lam_3 :: L<#body|i;i> -> (i, i) -> #body
+                lam_4 :: L<#body|i> -> i -> #body
+            """,
+        )
+
+    def test_expand_with_lambdas_large_number_intermediates(self):
+        level = 2
+        dslf = ns.DSLFactory(max_env_depth=level)
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("+", "(i, i) -> i", lambda x, y: x + y)
+        dslf.lambdas(require_arities=[level], max_type_depth=6)
+        dslf.prune_to(
+            ns.render_type(
+                ns.ArrowType((ns.AtomicType("i"),) * level, ns.AtomicType("i"))
+            )
+        )
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<i@0>
+                $1_0 :: V<i@1>
+                + :: (i, i) -> i
+                1 :: () -> i
+                lam :: L<#body|i;i> -> (i, i) -> #body
+            """,
+        )
+
+    def test_expand_with_lambdas_large_number_intermediates_large_domain(self):
+        level = 10
+        dslf = ns.DSLFactory(max_env_depth=level)
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("+", "(i, i) -> i", lambda x, y: x + y)
+        dslf.lambdas(require_arities=[level], max_type_depth=5)
+        dslf.prune_to(
+            ns.render_type(
+                ns.ArrowType((ns.AtomicType("i"),) * level, ns.AtomicType("i"))
+            )
+        )
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<i@0>
+                $1_0 :: V<i@1>
+                $2_0 :: V<i@2>
+                $3_0 :: V<i@3>
+                $4_0 :: V<i@4>
+                $5_0 :: V<i@5>
+                $6_0 :: V<i@6>
+                $7_0 :: V<i@7>
+                $8_0 :: V<i@8>
+                $9_0 :: V<i@9>
+                + :: (i, i) -> i
+                1 :: () -> i
+                lam :: L<#body|i;i;i;i;i;i;i;i;i;i> -> (i, i, i, i, i, i, i, i, i, i) -> #body
+            """,
+        )
