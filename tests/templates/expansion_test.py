083390dc92d7f05c39a10a58b1ff0e03ee12edea
Kavi Gupta <kavi@berkeley.edu>
2025-10-17 16:18:11 -0400

    faster expansions (#341)

--- tests/templates/expansion_test.py
+++ tests/templates/expansion_test.py
@@ -309,24 +309,13 @@ class TestDSLExpand(unittest.TestCase):
             dsl.render(),
             """
             + :: i -> i -> i
-            first_0 :: (#a, ((i, i) -> i) -> (i, i) -> i) -> #a
-            first_1 :: (#a, ((i, i) -> i) -> i -> i) -> #a
-            first_10 :: (#a, (i, i -> i) -> i -> i) -> #a
-            first_11 :: (#a, (i, i -> i) -> i) -> #a
-            first_12 :: (#a, (i, i) -> i -> i) -> #a
-            first_13 :: (#a, (i, i) -> i) -> #a
-            first_14 :: (#a, i -> (i, i) -> i) -> #a
-            first_15 :: (#a, i -> i -> i) -> #a
-            first_16 :: (#a, i -> i) -> #a
-            first_17 :: (#a, i) -> #a
-            first_2 :: (#a, ((i, i) -> i) -> i) -> #a
-            first_3 :: (#a, (i -> i, i -> i) -> i -> i) -> #a
-            first_4 :: (#a, (i -> i, i -> i) -> i) -> #a
-            first_5 :: (#a, (i -> i, i) -> i -> i) -> #a
-            first_6 :: (#a, (i -> i, i) -> i) -> #a
-            first_7 :: (#a, (i -> i) -> (i, i) -> i) -> #a
-            first_8 :: (#a, (i -> i) -> i -> i) -> #a
-            first_9 :: (#a, (i -> i) -> i) -> #a
+            first_0 :: (#a, (i -> i, i -> i) -> i -> i) -> #a
+            first_1 :: (#a, (i -> i, i) -> i -> i) -> #a
+            first_2 :: (#a, (i, i -> i) -> i) -> #a
+            first_3 :: (#a, (i, i) -> i) -> #a
+            first_4 :: (#a, i -> i -> i) -> #a
+            first_5 :: (#a, i -> i) -> #a
+            first_6 :: (#a, i) -> #a
             """,
         )
 
@@ -354,47 +343,14 @@ class TestDSLExpand(unittest.TestCase):
             self,
             dsl.render(),
             """
-                1 :: () -> i
-                ite_0 :: (b, () -> () -> () -> b, #a, #a) -> #a
-                ite_1 :: (b, () -> () -> () -> i, #a, #a) -> #a
-                ite_2 :: (b, () -> () -> b, #a, #a) -> #a
-                ite_3 :: (b, () -> () -> i, #a, #a) -> #a
-                ite_4 :: (b, () -> b, #a, #a) -> #a
-                ite_5 :: (b, () -> i, #a, #a) -> #a
-                ite_6 :: (b, (b, b, b, b) -> b, #a, #a) -> #a
-                ite_7 :: (b, (b, b, b, b) -> i, #a, #a) -> #a
-                ite_8 :: (b, (b, b, b, i) -> b, #a, #a) -> #a
-                ite_9 :: (b, (b, b, b, i) -> i, #a, #a) -> #a
-                ite_10 :: (b, (b, b, i, b) -> b, #a, #a) -> #a
-                ite_11 :: (b, (b, b, i, b) -> i, #a, #a) -> #a
-                ite_12 :: (b, (b, b, i, i) -> b, #a, #a) -> #a
-                ite_13 :: (b, (b, b, i, i) -> i, #a, #a) -> #a
-                ite_14 :: (b, (b, i, b, b) -> b, #a, #a) -> #a
-                ite_15 :: (b, (b, i, b, b) -> i, #a, #a) -> #a
-                ite_16 :: (b, (b, i, b, i) -> b, #a, #a) -> #a
-                ite_17 :: (b, (b, i, b, i) -> i, #a, #a) -> #a
-                ite_18 :: (b, (b, i, i, b) -> b, #a, #a) -> #a
-                ite_19 :: (b, (b, i, i, b) -> i, #a, #a) -> #a
-                ite_20 :: (b, (b, i, i, i) -> b, #a, #a) -> #a
-                ite_21 :: (b, (b, i, i, i) -> i, #a, #a) -> #a
-                ite_22 :: (b, (i, b, b, b) -> b, #a, #a) -> #a
-                ite_23 :: (b, (i, b, b, b) -> i, #a, #a) -> #a
-                ite_24 :: (b, (i, b, b, i) -> b, #a, #a) -> #a
-                ite_25 :: (b, (i, b, b, i) -> i, #a, #a) -> #a
-                ite_26 :: (b, (i, b, i, b) -> b, #a, #a) -> #a
-                ite_27 :: (b, (i, b, i, b) -> i, #a, #a) -> #a
-                ite_28 :: (b, (i, b, i, i) -> b, #a, #a) -> #a
-                ite_29 :: (b, (i, b, i, i) -> i, #a, #a) -> #a
-                ite_30 :: (b, (i, i, b, b) -> b, #a, #a) -> #a
-                ite_31 :: (b, (i, i, b, b) -> i, #a, #a) -> #a
-                ite_32 :: (b, (i, i, b, i) -> b, #a, #a) -> #a
-                ite_33 :: (b, (i, i, b, i) -> i, #a, #a) -> #a
-                ite_34 :: (b, (i, i, i, b) -> b, #a, #a) -> #a
-                ite_35 :: (b, (i, i, i, b) -> i, #a, #a) -> #a
-                ite_36 :: (b, (i, i, i, i) -> b, #a, #a) -> #a
-                ite_37 :: (b, (i, i, i, i) -> i, #a, #a) -> #a
-                ite_38 :: (b, b, #a, #a) -> #a
-                ite_39 :: (b, i, #a, #a) -> #a
+            1 :: () -> i
+            ite_0 :: (b, () -> i, #a, #a) -> #a
+            ite_1 :: (b, (b, b, b, b) -> b, #a, #a) -> #a
+            ite_2 :: (b, (b, b, i, i) -> i, #a, #a) -> #a
+            ite_3 :: (b, (b, i, b, b) -> b, #a, #a) -> #a
+            ite_4 :: (b, (b, i, i, i) -> i, #a, #a) -> #a
+            ite_5 :: (b, b, #a, #a) -> #a
+            ite_6 :: (b, i, #a, #a) -> #a
             """,
         )
 
@@ -445,43 +401,24 @@ class TestDSLExpand(unittest.TestCase):
             self,
             dsl.render(),
             """
-                $0_0 :: V<() -> () -> i@0>
-                $0_1 :: V<() -> i@0>
-                $0_2 :: V<(i, i) -> i@0>
-                $0_3 :: V<i -> i@0>
-                $0_4 :: V<i@0>
-                $1_0 :: V<() -> () -> i@1>
-                $1_1 :: V<() -> i@1>
-                $1_2 :: V<(i, i) -> i@1>
-                $1_3 :: V<i -> i@1>
-                $1_4 :: V<i@1>
-                $2_0 :: V<() -> () -> i@2>
-                $2_1 :: V<() -> i@2>
-                $2_2 :: V<(i, i) -> i@2>
-                $2_3 :: V<i -> i@2>
-                $2_4 :: V<i@2>
-                $3_0 :: V<() -> () -> i@3>
-                $3_1 :: V<() -> i@3>
-                $3_2 :: V<(i, i) -> i@3>
-                $3_3 :: V<i -> i@3>
-                $3_4 :: V<i@3>
+                $0_0 :: V<() -> i@0>
+                $0_1 :: V<i -> i@0>
+                $0_2 :: V<i@0>
+                $1_0 :: V<() -> i@1>
+                $1_1 :: V<i -> i@1>
+                $1_2 :: V<i@1>
+                $2_0 :: V<() -> i@2>
+                $2_1 :: V<i -> i@2>
+                $2_2 :: V<i@2>
+                $3_0 :: V<() -> i@3>
+                $3_1 :: V<i -> i@3>
+                $3_2 :: V<i@3>
                 1 :: () -> i
                 id :: #a -> #a
                 lam_0 :: L<#body|> -> () -> #body
-                lam_1 :: L<#body|() -> () -> i> -> (() -> () -> i) -> #body
-                lam_10 :: L<#body|i -> i> -> (i -> i) -> #body
-                lam_11 :: L<#body|i;() -> i> -> (i, () -> i) -> #body
-                lam_12 :: L<#body|i;i -> i> -> (i, i -> i) -> #body
-                lam_13 :: L<#body|i;i> -> (i, i) -> #body
-                lam_14 :: L<#body|i> -> i -> #body
-                lam_2 :: L<#body|() -> i;() -> i> -> (() -> i, () -> i) -> #body
-                lam_3 :: L<#body|() -> i;i -> i> -> (() -> i, i -> i) -> #body
-                lam_4 :: L<#body|() -> i;i> -> (() -> i, i) -> #body
-                lam_5 :: L<#body|() -> i> -> (() -> i) -> #body
-                lam_6 :: L<#body|(i, i) -> i> -> ((i, i) -> i) -> #body
-                lam_7 :: L<#body|i -> i;() -> i> -> (i -> i, () -> i) -> #body
-                lam_8 :: L<#body|i -> i;i -> i> -> (i -> i, i -> i) -> #body
-                lam_9 :: L<#body|i -> i;i> -> (i -> i, i) -> #body
+                lam_1 :: L<#body|() -> i> -> (() -> i) -> #body
+                lam_2 :: L<#body|i -> i> -> (i -> i) -> #body
+                lam_3 :: L<#body|i> -> i -> #body
             """,
         )
 
@@ -519,7 +456,7 @@ class TestDSLExpand(unittest.TestCase):
         dslf = ns.DSLFactory(max_env_depth=level)
         dslf.production("1", "() -> i", lambda: 1)
         dslf.production("+", "(i, i) -> i", lambda x, y: x + y)
-        dslf.lambdas(require_arities=[level], max_type_depth=6)
+        dslf.lambdas(max_type_depth=6)
         dslf.prune_to(
             ns.render_type(
                 ns.ArrowType((ns.AtomicType("i"),) * level, ns.AtomicType("i"))
@@ -543,7 +480,7 @@ class TestDSLExpand(unittest.TestCase):
         dslf = ns.DSLFactory(max_env_depth=level)
         dslf.production("1", "() -> i", lambda: 1)
         dslf.production("+", "(i, i) -> i", lambda x, y: x + y)
-        dslf.lambdas(require_arities=[level], max_type_depth=5)
+        dslf.lambdas(max_type_depth=5)
         dslf.prune_to(
             ns.render_type(
                 ns.ArrowType((ns.AtomicType("i"),) * level, ns.AtomicType("i"))
