7bac2cd360759cebe6ed3434193ac387ad977d67
Kavi Gupta <kavi@berkeley.edu>
2025-10-17 13:44:25 -0400

    expand lambdas in test (#340)

--- tests/templates/expansion_test.py
+++ tests/templates/expansion_test.py
@@ -1,3 +1,6 @@
+# pylint: disable=duplicate-code
+# we don't care about duplicate code in test outputs
+
 import unittest
 
 import neurosym as ns
@@ -411,5 +414,158 @@ class TestDSLExpand(unittest.TestCase):
                 1 :: () -> i
                 1f :: () -> f
                 + :: %num -> %num -> %num
+            """,
+        )
+
+    def test_expand_with_lambdas(self):
+        dslf = ns.DSLFactory()
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("id", "#a -> #a", lambda x: x)
+        dslf.lambdas()
+        dslf.prune_to("i -> i")
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<i@0>
+                1 :: () -> i
+                id :: #a -> #a
+                lam :: L<#body|i> -> i -> #body
+            """,
+        )
+
+    def test_expand_with_lambdas_no_prune(self):
+        dslf = ns.DSLFactory()
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("id", "#a -> #a", lambda x: x)
+        dslf.lambdas()
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<() -> () -> i@0>
+                $0_1 :: V<() -> i@0>
+                $0_2 :: V<(i, i) -> i@0>
+                $0_3 :: V<i -> i@0>
+                $0_4 :: V<i@0>
+                $1_0 :: V<() -> () -> i@1>
+                $1_1 :: V<() -> i@1>
+                $1_2 :: V<(i, i) -> i@1>
+                $1_3 :: V<i -> i@1>
+                $1_4 :: V<i@1>
+                $2_0 :: V<() -> () -> i@2>
+                $2_1 :: V<() -> i@2>
+                $2_2 :: V<(i, i) -> i@2>
+                $2_3 :: V<i -> i@2>
+                $2_4 :: V<i@2>
+                $3_0 :: V<() -> () -> i@3>
+                $3_1 :: V<() -> i@3>
+                $3_2 :: V<(i, i) -> i@3>
+                $3_3 :: V<i -> i@3>
+                $3_4 :: V<i@3>
+                1 :: () -> i
+                id :: #a -> #a
+                lam_0 :: L<#body|> -> () -> #body
+                lam_1 :: L<#body|() -> () -> i> -> (() -> () -> i) -> #body
+                lam_10 :: L<#body|i -> i> -> (i -> i) -> #body
+                lam_11 :: L<#body|i;() -> i> -> (i, () -> i) -> #body
+                lam_12 :: L<#body|i;i -> i> -> (i, i -> i) -> #body
+                lam_13 :: L<#body|i;i> -> (i, i) -> #body
+                lam_14 :: L<#body|i> -> i -> #body
+                lam_2 :: L<#body|() -> i;() -> i> -> (() -> i, () -> i) -> #body
+                lam_3 :: L<#body|() -> i;i -> i> -> (() -> i, i -> i) -> #body
+                lam_4 :: L<#body|() -> i;i> -> (() -> i, i) -> #body
+                lam_5 :: L<#body|() -> i> -> (() -> i) -> #body
+                lam_6 :: L<#body|(i, i) -> i> -> ((i, i) -> i) -> #body
+                lam_7 :: L<#body|i -> i;() -> i> -> (i -> i, () -> i) -> #body
+                lam_8 :: L<#body|i -> i;i -> i> -> (i -> i, i -> i) -> #body
+                lam_9 :: L<#body|i -> i;i> -> (i -> i, i) -> #body
+            """,
+        )
+
+    def test_expand_with_lambdas_in_not_top_level(self):
+        dslf = ns.DSLFactory()
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("id", "((i, #a) -> #a) -> i", lambda x, y: x)
+        dslf.lambdas()
+        dslf.prune_to("i -> i")
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<() -> i@0>
+                $0_1 :: V<i -> i@0>
+                $0_2 :: V<i@0>
+                $1_2 :: V<i@1>
+                $2_2 :: V<i@2>
+                $3_2 :: V<i@3>
+                1 :: () -> i
+                id_0 :: ((i, () -> i) -> () -> i) -> i
+                id_1 :: ((i, i -> i) -> i -> i) -> i
+                id_2 :: ((i, i) -> i) -> i
+                lam_0 :: L<#body|> -> () -> #body
+                lam_1 :: L<#body|i;() -> i> -> (i, () -> i) -> #body
+                lam_2 :: L<#body|i;i -> i> -> (i, i -> i) -> #body
+                lam_3 :: L<#body|i;i> -> (i, i) -> #body
+                lam_4 :: L<#body|i> -> i -> #body
+            """,
+        )
+
+    def test_expand_with_lambdas_large_number_intermediates(self):
+        level = 2
+        dslf = ns.DSLFactory(max_env_depth=level)
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("+", "(i, i) -> i", lambda x, y: x + y)
+        dslf.lambdas(require_arities=[level], max_type_depth=6)
+        dslf.prune_to(
+            ns.render_type(
+                ns.ArrowType((ns.AtomicType("i"),) * level, ns.AtomicType("i"))
+            )
+        )
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<i@0>
+                $1_0 :: V<i@1>
+                + :: (i, i) -> i
+                1 :: () -> i
+                lam :: L<#body|i;i> -> (i, i) -> #body
+            """,
+        )
+
+    def test_expand_with_lambdas_large_number_intermediates_large_domain(self):
+        level = 10
+        dslf = ns.DSLFactory(max_env_depth=level)
+        dslf.production("1", "() -> i", lambda: 1)
+        dslf.production("+", "(i, i) -> i", lambda x, y: x + y)
+        dslf.lambdas(require_arities=[level], max_type_depth=5)
+        dslf.prune_to(
+            ns.render_type(
+                ns.ArrowType((ns.AtomicType("i"),) * level, ns.AtomicType("i"))
+            )
+        )
+        dsl = dslf.finalize()
+        assertDSL(
+            self,
+            dsl.render(),
+            """
+                $0_0 :: V<i@0>
+                $1_0 :: V<i@1>
+                $2_0 :: V<i@2>
+                $3_0 :: V<i@3>
+                $4_0 :: V<i@4>
+                $5_0 :: V<i@5>
+                $6_0 :: V<i@6>
+                $7_0 :: V<i@7>
+                $8_0 :: V<i@8>
+                $9_0 :: V<i@9>
+                + :: (i, i) -> i
+                1 :: () -> i
+                lam :: L<#body|i;i;i;i;i;i;i;i;i;i> -> (i, i, i, i, i, i, i, i, i, i) -> #body
             """,
         )
